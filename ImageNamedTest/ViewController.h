abf78ad4340bf3d18d0876d91a26962e28bda072
Daniel Tull <dt@danieltull.co.uk>
2013-01-18 14:24:53 +0000

    Setup view controller

---
+++ ImageNamedTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ImageNamedTest
+//
+//  Created by Daniel Tull on 18.01.2013.
+//  Copyright (c) 2013 Daniel Tull. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ ImageNamedTest/ViewController.m
@@ -0,0 +1,38 @@
+//
+//  ViewController.m
+//  ImageNamedTest
+//
+//  Created by Daniel Tull on 18.01.2013.
+//  Copyright (c) 2013 Daniel Tull. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
